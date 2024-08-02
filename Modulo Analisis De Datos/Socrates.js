373eb9abfa084b55aa2ccef7b173af015bdddf46
Emiliano-Agurcia <110260889+Emiliano-Agurcia@users.noreply.github.com>
2024-08-02 10:08:44 -0600

    Socrates: Navbar; Socrates.js: No existía getMessage() xd

--- Modulo Analisis De Datos/Socrates.js
+++ Modulo Analisis De Datos/Socrates.js
@@ -147,10 +147,10 @@ function sendMessage(message, imageSrc) {
 
     //Mensaje de espera de respuesta queda abajo
     messageList.insertBefore(loadingMsg, null);
-
+    
+    //Procesar y Enviar Respuesta como Encuestador
     const url = 'http://54.145.222.179:3000/communicateS/';
 
-    //Procesar y Enviar Respuesta como Encuestador
     axios.post(url, { prompt: message , hash: hash }, {
         headers:{
             'Content-Type': 'multipart/form-data',
@@ -165,8 +165,6 @@ function sendMessage(message, imageSrc) {
 
             getMessage(farewellMessage, null);
         
-
-
         }else{
             getMessage(data.response, null);
             loadingMsg.style.display = 'none';
@@ -205,6 +203,106 @@ function sendMessage(message, imageSrc) {
     // });
 }
 
+//Función para recibir un mensaje de encuestador (TEST)
+function getMessage(message, imageSrc) {
+    const Feed = document.getElementById('Feed');//Validar Feed Vacío
+    const emptyFeed = document.getElementById('Empty-Feed');
+    if (Feed.style.display === 'none') {
+        emptyFeed.style.display = 'none';
+        Feed.style.display = 'flex';
+    }
+
+    let options = {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+
+        hour: '2-digit',
+        minute: '2-digit',
+
+        hour12: true
+    };
+
+    const messageList = document.getElementById('Message-List');
+    const li = document.createElement('li');
+    li.className = 'd-flex my-3';
+
+    const BotIMG_Div = document.createElement('div');
+    BotIMG_Div.className = 'BotIMG-Div d-flex justify-content-end align-items-center align-content-center align-self-end';
+    BotIMG_Div.style.width = '50px';
+    BotIMG_Div.style.height = '100%';
+    BotIMG_Div.style.paddingRight = '2px';
+    BotIMG_Div.style.paddingLeft = '0px';
+
+    const BotIMG_Cont = document.createElement('div');
+    BotIMG_Cont.className = 'BotIMG-Cont text-truncate d-flex justify-content-center align-items-center align-content-center';
+    BotIMG_Cont.style.background = '#d1d1d1';
+
+    const BotIMG = document.createElement('img');
+    BotIMG.src = imgPP;
+    BotIMG.style.maxHeight = '100%';
+
+    const card = document.createElement('div');
+    card.className = 'card d-inline-block';
+    card.style.maxWidth = '75%';
+    card.style.borderRadius = '15px';
+    card.style.borderBottomLeftRadius = '0px';
+    card.style.borderBottomWidth = 'medium';
+    card.style.borderColor = '#C2681A';
+    card.style.background = 'var(--bs-CR-orange-2)';
+
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body text-break text-center d-flex flex-column p-2';
+
+    const img = document.createElement('img');
+    if (imageSrc) {
+        img.className = 'img-fluid d-flex order-1 mx-auto mb-2'
+        img.src = imageSrc;
+        img.style.maxHeight = '18rem';
+        img.style.height = 'auto';
+        img.style.minHeight = '9rem';
+        img.style.paddingLeft = '0px';
+        cardBody.appendChild(img);
+    }
+
+    const p = document.createElement('p');
+    p.className = 'text-break text-start d-flex order-2 card-text';
+    p.style.color = '#f0f0f0';
+    p.style.fontFamily = "'IBM Plex Sans', sans-serif";
+    p.style.marginBottom = "6px";
+    p.textContent = message;
+    p.innerHTML = message.replace(/\n/g, '<br>').replace(/ {2,}/g, match => '&nbsp;'.repeat(match.length));//registra el newline y espacios
+
+    const h4 = document.createElement('h4');
+    h4.className = 'd-flex align-self-start justify-content-end order-3 card-subtitle text-end';
+    h4.style.marginTop = '0px';
+    h4.style.color = '#555155';
+    h4.style.fontFamily = "'League Spartan', sans-serif";
+    h4.textContent = new Intl.DateTimeFormat('es-419', options).format(new Date());
+    h4.textContent = h4.textContent.replace('a.\u00A0m.', 'AM').replace('p.\u00A0m.', 'PM');
+
+    BotIMG_Cont.appendChild(BotIMG);
+
+    BotIMG_Div.appendChild(BotIMG_Cont);
+
+    cardBody.appendChild(p);
+    cardBody.appendChild(h4);
+    card.appendChild(cardBody);
+
+    li.appendChild(BotIMG_Div);
+    li.appendChild(card);
+
+    messageList.appendChild(li);
+
+    //Scroll automático hacia abajo cuando se envía un mensaje nuevo
+    const scrollPanel = document.getElementById('Feed-BG');
+    scrollPanel.scrollTop = scrollPanel.scrollHeight;
+
+    //Mensaje de espera de respuesta queda abajo
+    let loadingMsg = document.getElementById('Typing-Msg');
+    messageList.insertBefore(loadingMsg, null);
+}
+
 //Funciones cambiar colores de botones al soltar botón (móviles)
 document.getElementById('btSend-Cont').addEventListener('touchstart', function () {
     btSend = document.getElementById('btSend');
@@ -238,8 +336,6 @@ function load() {
         // poner la cosa con id"Bot-Status" como "Conectado"
         document.getElementById('Bot-Status').innerText = 'Conectado';
 
-
-
     }).catch((error) => {
         console.log('Error:', error);
     });
@@ -247,21 +343,14 @@ function load() {
 
 //Función Cargar Entrevistador
 function loadInterviewer(){
-    
         const nombre = 'Socrates';
         const imagen = 'assets/img/Socrates.jpg';
         const interviewerGreeting = `Saludos, buscador de verdades. Soy Sócrates, tu guía en este viaje hacia el conocimiento. A través del arte de la dialéctica, juntos desentrañaremos los misterios ocultos en los datos que nos rodean, pues es por medio del diálogo y la reflexión profunda que alcanzamos la verdadera sabiduría. Pregunta sin temor, y hallaremos respuestas que iluminen el camino hacia la comprensión.`;
 
-
-
         const formContainerI = document.getElementById('overlay');
-
-
 
         document.getElementById('Bot-Name').innerText = nombre; 
         const formContainer = document.createElement('div');
-
-
 
         formContainer.innerHTML = `
         <div id="overlayContent" class="text-wrap">
@@ -286,5 +375,4 @@ function loadInterviewer(){
             document.getElementById('overlay').style.display = 'none';
             load();
         });
-
 }
