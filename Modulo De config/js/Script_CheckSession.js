12275510417db0d43102bc1627c70c24403c2063
Wilmer Zuniga <wilmerzuniga.ant@gmail.com>
2024-08-19 22:13:23 -0600

    bug al generar estudio nuevo

--- Modulo De config/js/Script_CheckSession.js
+++ Modulo De config/js/Script_CheckSession.js
@@ -15,7 +15,6 @@ async function checkSession(){
     } catch (error) {
         console.log('error check session', error);
     }
-//coso para probar
 }
 
 document.addEventListener('DOMContentLoaded', function() {
