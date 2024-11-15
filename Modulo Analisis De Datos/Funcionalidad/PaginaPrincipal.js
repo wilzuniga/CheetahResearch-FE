edd75f2947cb06fcb513d6df311d42bd3afaa53b
Wilmer Zuniga <wilmerzuniga.ant@gmail.com>
2024-11-14 23:27:23 -0600

    overlay shown

--- Modulo Analisis De Datos/Funcionalidad/PaginaPrincipal.js
+++ Modulo Analisis De Datos/Funcionalidad/PaginaPrincipal.js
@@ -10,7 +10,7 @@ function otp(study_id) {
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
-            border-radius: 25px;
+            border-radius: 10px;
             text-align: center;
             text-color: black;
             padding: 20px;
