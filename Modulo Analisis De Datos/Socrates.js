2d9ba42f14b2d996922c3123bf36f3fd50a85567
Emiliano-Agurcia <110260889+Emiliano-Agurcia@users.noreply.github.com>
2025-04-11 00:39:27 -0600

    Colores en Socrates

--- Modulo Analisis De Datos/Socrates.js
+++ Modulo Analisis De Datos/Socrates.js
@@ -348,6 +348,7 @@ function load() {
     const url = 'https://api.cheetah-research.ai/analysis/startS/';
 
     const study_id = new URLSearchParams(window.location.search).get('id');
+    setColorsFromAPI(study_id);//Setea colores
     // console.log(study_id);
     axios.post(url, { study_id: study_id }, {
         headers: {
@@ -482,4 +483,54 @@ function AgregarPreguntas() {
         .catch(error => {
             console.error('Error al enviar los datos:', error);
         });
+}
+
+//Colores
+function setColorsFromAPI(studyId) {
+    const url = 'https://api.cheetah-research.ai/configuration/info_study/' + studyId;
+    return axios.get(url)
+        .then(response => {
+            const colors = {
+                color1: response.data.primary_color,
+                color2: response.data.secondary_color
+            };
+
+            applyColors(colors);
+
+            return colors;
+        })
+        .catch(error => {
+            console.error('Error capturando colores desde API:', error);
+            return { color1: null, color2: null };
+        });
+}
+
+function applyColors(colors) {//Colors es un array
+    if (colors.color1) {
+        document.documentElement.style.setProperty('--bs-CR-orange', colors.color1);
+
+        document.documentElement.style.setProperty('--bs-CR-orange-2', brightColorVariant(colors.color1));
+    }
+    if (colors.color2) {
+        document.documentElement.style.setProperty('--bs-CR-gray', colors.color2);
+
+        document.documentElement.style.setProperty('--bs-CR-gray-dark', darkColorVariant(colors.color2));
+    }
+}
+function darkColorVariant (color) {
+    return adjustColor(color, -10);
+}
+function brightColorVariant (color) {
+    return adjustColor(color, 10);
+}
+function adjustColor(color, percent) {//Funcion loca de chatsito
+    const num = parseInt(color.slice(1), 16),
+          amt = Math.round(2.55 * percent),
+          R = (num >> 16) + amt,
+          G = (num >> 8 & 0x00FF) + amt,
+          B = (num & 0x0000FF) + amt;
+    return `#${(0x1000000 + (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 + 
+                (G < 255 ? (G < 1 ? 0 : G) : 255) * 0x100 + 
+                (B < 255 ? (B < 1 ? 0 : B) : 255))
+                .toString(16).slice(1).toUpperCase()}`;
 }
